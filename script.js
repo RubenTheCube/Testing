6cacb420aadb04d7ac591800e93861a7665cfb6e
RubenTheCube <RubenTheCube@users.noreply.github.com>
2017-03-07 23:53:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+$(document).ready(function() {
 
+	//$(".personalTable").hide();
+
+	function slider() {
+    	var currentSlide = $(".active-slide");
+   		var nextSlide = currentSlide.next();
+		if (nextSlide.length == 0){
+	   		nextSlide = $(".slide").first();
+	 	}
+		currentSlide.fadeOut(1000).removeClass("active-slide");
+	   	nextSlide.fadeIn(1000).addClass("active-slide");
+	}
+
+	var timer = setInterval(slider, 6000);
+
+
+	$("#clickme").click(function(){
+		$(".personalTable").toggle(1000);
+	});
+
+	$("#clickme2").click(function() {
+		$(".personalTable").hide(1000);
+	})
+});
